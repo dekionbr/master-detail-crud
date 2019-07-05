82548bfebe0192417f7c5141dff097c66d66999f
Julio Silveira <julio.silveira@fulllab.com.br>
2019-07-05 14:38:06 -0300

    feacture manager category

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Category, CategoryService } from '../shared/';
+import { Alert } from 'selenium-webdriver';
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
+    this.categoryService
+      .getAll()
+      .subscribe(
+        categories => (this.categories = categories),
+        error => alert('Error ao carregar a lista')
+      );
   }
 
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja realmente excluir esse item?');
+
+    if (mustDelete) {
+      this.categoryService
+        .delete(category.id)
+        .subscribe(
+          () =>
+            (this.categories = this.categories.filter(el => el !== category)),
+          () => alert('Erro ao tentar excluir')
+        );
+    }
+  }
 }
---
+++ src/app/pages/categories/shared/index.ts
@@ -0,0 +1,4 @@
+import { Category } from './category.model';
+import { CategoryService } from './category.service';
+
+export { Category, CategoryService };
