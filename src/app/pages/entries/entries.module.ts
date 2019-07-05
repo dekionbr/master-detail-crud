57b90ec5e6b7666b6f1ae49bd96be8a121b7cf81
Julio Silveira <julio.silveira@fulllab.com.br>
2019-07-05 17:43:34 -0300

    adding entry pages components

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
+import { EntryFormComponent } from './entry-form/entry-form.component';
+import { EntryListComponent } from './entry-list/entry-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [EntryFormComponent, EntryListComponent],
   imports: [
     CommonModule,
     EntriesRoutingModule
